4977bb6126b5eee0667cd552fec2b447c5663495
nahidniloy14 <nahidniloy894@gmail.com>
2023-05-28 00:09:12 +0600

    Signin Attempt

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-serviceObject = Service("C:\Driver\chromedriver.exe")
+serviceObject = Service("C:\Driver\chromedriver107.exe")
 
 driver = webdriver.Chrome(service=serviceObject)
 
-
 driver.get("https://wordpress.com/log-in/")
-
-
+driver.find_element(By.CLASS_NAME, "form-text-input").send_keys("nahidniloy894@gmail.com")
+driver.find_element(By.CLASS_NAME, "button form-button is-primary").click()
