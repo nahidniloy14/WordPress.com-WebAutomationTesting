cc58fd9574b2d1a3f0bb381ada9f706f781f7dcf
nahidniloy14 <nahidniloy894@gmail.com>
2023-05-28 01:45:51 +0600

    Sign in to the wordpress

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-serviceObject = Service("C:\Driver\chromedriver107.exe")
+serviceObject = Service("C:\Driver\chromedriver.exe")
 
 driver = webdriver.Chrome(service=serviceObject)
 
 driver.get("https://wordpress.com/log-in/")
-driver.find_element(By.CLASS_NAME, "form-text-input").send_keys("nahidniloy894@gmail.com")
-driver.find_element(By.CLASS_NAME, "button form-button is-primary").click()
+# Sign up
+# driver.find_element(By.CLASS_NAME, "form-text-input").send_keys("nahidniloy894@gmail.com")
+# time.sleep(8)
+# driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+#sign in with Google
+driver.find_element(By.CLASS_NAME,"social-buttons__service-name").click()
+time.sleep(3)
+driver.find_element(By.ID,"identifierId").send_keys("nahidniloy894@gmail.com")
+driver.find_element(By.XPATH,"//div[@id='view_container']/div/div/div[2]/div/div[2]/div/div[1]/div/div").click()
